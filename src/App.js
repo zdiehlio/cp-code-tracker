e0f2430de0e0e3c0999ef854e086dafdf52744a9
zdiehlio <zachary.diehl@gmail.com>
2018-12-11 18:22:58 -0500

    deploying

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
-import Menu from '@material-ui/core/Menu'
+import Menu from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
+import Drawer from '@material-ui/core/Drawer'
 import './App.css';
 
 class App extends Component {
+  state = {
+    drawer: false,
+  }
+
+  handleClick = () => {
+    this.setState({ drawer: this.state.drawer === false ? true : false})
+    console.log(this.state.drawer)
+  }
   render() {
     return (
       <div className="App">
         <AppBar position="static" >
           <ToolBar>
-            <IconButton>
+            <IconButton onClick={this.handleClick}>
               <Menu />  
             </IconButton>
             <h3>CP Code Tracker</h3>
           </ToolBar>
         </AppBar>
+        <Drawer open={this.state.drawer} onClose={this.handleClick} >
+          <div 
+            tabIndex={0}
+            role='button'
+            onClick={this.handleClick}
+            onKeyDown={this.handleClick}
+          >
+          Some Stuff
+          </div>
+        </Drawer>
       </div>
     );
   }
