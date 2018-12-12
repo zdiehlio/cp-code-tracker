05b668d0c75e772117001ac91d70de010b76bfd1
zdiehlio <zachary.diehl@gmail.com>
2018-12-12 09:02:41 -0500

    set up communication with api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import ToolBar from '@material-ui/core/Toolbar'
 import Menu from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
 import Drawer from '@material-ui/core/Drawer'
+import superagent from 'superagent'
 import './App.css';
 
 class App extends Component {
   state = {
     drawer: false,
+  }
+
+  componentWillMount(){
+    superagent.get(`https://damp-beyond-68108.herokuapp.com/`)
+      .then(res => console.log(res.text))
   }
 
   handleClick = () => {
